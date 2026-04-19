c3a0bafa27ef65b2d02328cde55c5b1558750a0e
notmtri <nguyenhoangminhtri2k8@gmail.com>
2026-04-19 17:06:29 +0700

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import HomePage from './components/HomePage';
 import ProductsPage from './components/ProductsPage';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 interface Product {
   id: string;
